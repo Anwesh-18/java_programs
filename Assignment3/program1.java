df77597f9929e5eaad0bb7e9dbaa3697fb6703e7
Anwesh-18 <cse.24bcsh15@silicon.ac.in>
2025-09-09 22:58:50 +0530

    Assignment 3 completed

---
+++ Assignment3/program1.java
@@ -0,0 +1,27 @@
+package Assignment3;
+
+import java.util.Scanner;
+
+public class program1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        System.out.print("Enter the array elements: ");
+
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        System.out.print("Enter the element to search: ");
+        int key = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] == key) {
+                System.out.println(key + " was found at index " + i);
+                return;
+            }
+        }
+        System.out.println(key + " was not found in the array");
+    }
+}
---
+++ Assignment3/program4.java
@@ -0,0 +1,10 @@
+package Assignment3;
+
+public class program4 {
+     public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int res = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + res);
+    }
+}
